8883ca12d54a7198b8fa78394ab0530416481945
Aleksander Alafuzoff <aleksander.alafuzoff@gmail.com>
2017-09-16 18:01:51 +0300

    Korjattu sql-komennot ja niiden oikeudet.

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,17 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja (tunnus, salasana, nimi, email, oikeudet, status, lisayspvm) VALUES 
+  ('Yrmi', 'salainen', 'Yrmi Ylläpitäjä', 'yrmi.yllapitaja@yliopisto.fi', 'yllapitaja', 'true', '2017-09-01'), 
+  ('linda', 'salasana', 'Linda Lupavastaava', 'linda.lupavastaava@yliopisto.fi', 'lupavastaava', 'true', '2017-09-10'),
+  ('bella', '12345', 'Bella Biologi', 'bella.biologi@yliopisto.fi', 'tutkija', 'true', '2017-09-15');
+  
+INSERT INTO Elainkoelupa (tunnus, nimi, alkupvm, loppupvm, vastuuhlo_id) VALUES
+  ('ESAVI-12345', 'Rottien levitointitutkimus', '2017-06-01', '2019-06-01', (SELECT id FROM Kayttaja WHERE tunnus = 'linda'));
+  
+INSERT INTO Elainkoe (suorituspvm, laji, ika, lukumaara, lisatiedot, lupa_id) VALUES
+  ('2017-09-15', 'rotta', 'Aikuinen', 3, 'Levitointi onnistunut!', (SELECT id FROM Elainkoelupa WHERE tunnus = 'ESAVI-12345'));
+  
+INSERT INTO Koeosallistuminen (koe_id, kayttaja_id) VALUES
+  ((SELECT id FROM Elainkoe WHERE lisatiedot = 'Levitointi onnistunut!'), (SELECT id FROM Kayttaja WHERE tunnus = 'bella')),
+  ((SELECT id FROM Elainkoe WHERE lisatiedot = 'Levitointi onnistunut!'), (SELECT id FROM Kayttaja WHERE tunnus = 'linda'));
+  
+  
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,39 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TYPE kayttajanoikeudet AS ENUM ('tutkija', 'lupavastaava', 'yllapitaja');
+CREATE TYPE elainlaji AS ENUM ('rotta', 'hiiri');
+
+CREATE TABLE Kayttaja (
+  id        SERIAL PRIMARY KEY,
+  tunnus    VARCHAR(20) NOT NULL,
+  salasana  VARCHAR(20) NOT NULL,
+  nimi      VARCHAR(100) NOT NULL,
+  email     VARCHAR(100) NOT NULL,
+  oikeudet  kayttajanoikeudet,
+  status    BOOLEAN,
+  lisayspvm DATE
+);
+
+CREATE TABLE Elainkoelupa (
+  id           SERIAL PRIMARY KEY,
+  tunnus       VARCHAR(20) NOT NULL,
+  nimi         VARCHAR(100) NOT NULL,
+  alkupvm      DATE,
+  loppupvm     DATE,
+  vastuuhlo_id INTEGER REFERENCES Kayttaja(id)
+);
+
+CREATE TABLE Elainkoe (
+  id          SERIAL PRIMARY KEY,
+  suorituspvm DATE,
+  laji        elainlaji,
+  ika         VARCHAR(20),
+  lukumaara   INTEGER,
+  lisatiedot  VARCHAR(500),
+  lupa_id     INTEGER REFERENCES Elainkoelupa(id)
+);
+
+CREATE TABLE Koeosallistuminen (
+  koe_id      INTEGER REFERENCES Elainkoe(id),
+  kayttaja_id INTEGER REFERENCES Kayttaja(id),
+  PRIMARY KEY(koe_id, kayttaja_id)
+);
