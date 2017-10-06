087ed3e187f7949e1a3882abfe4684725c29dafd
Aleksander Alafuzoff <aleksander.alafuzoff@gmail.com>
2017-10-06 16:16:55 +0300

    Yhtenäistetty viestinäkymiä ja laajennettu käyttäjien tunnistautumista

--- lib/base_controller.php
+++ lib/base_controller.php
@@ -13,14 +13,21 @@
       return null;
     }
 
-    public static function check_logged_in(){
-      // Toteuta kirjautumisen tarkistus tähän.
-      // Jos käyttäjä ei ole kirjautunut sisään, ohjaa hänet toiselle sivulle (esim. kirjautumissivulle).
+    public static function check_logged_in() {
+      if(!isset($_SESSION['kayttaja'])) {
+        Redirect::to('/login', array('error' => 'Kirjaudu sisään käyttääksesi sovellusta'));
+      }
     }
 
-    public static function enforce_login() {
-      if(!isset($_SESSION['kayttaja'])) {
-        Redirect::to('/login');
+    public static function check_yllapitaja() {
+      if($_SESSION['oikeudet'] != 'yllapitaja') {
+        Redirect::to('/', array('error' => 'Vain ylläpitäjät käyttää tuota sivua!'));
+      }
+    }
+
+    public static function check_lupavastaava() {
+      if($_SESSION['oikeudet'] == 'tutkija') {
+        Redirect::to('/', array('error' => 'Vain lupavastaavat ja ylläpitäjät voivat käyttää tuota sivua!'));
       }
     }
 
