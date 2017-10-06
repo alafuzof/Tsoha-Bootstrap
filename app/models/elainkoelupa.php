513974f668f1b3357a88e13a5a378d7e23cc3d31
Aleksander Alafuzoff <aleksander.alafuzoff@gmail.com>
2017-10-06 17:23:34 +0300

    Tallennus/päivitys/poisto -toiminnallisuus Eläinkoelupa-malliin

--- app/models/elainkoelupa.php
+++ app/models/elainkoelupa.php
@@ -91,4 +91,68 @@
 
       return NULL;
     }
+
+    public static function find_by_kayttaja($kayttaja) {
+      $query = DB::connection()->prepare(
+        'SELECT l.id AS id, l.tunnus AS tunnus, l.nimi AS nimi, ' .
+        '       l.alkupvm AS alkupvm, l.loppupvm AS loppupvm, ' .
+        '       l.vastuuhlo_id AS vastuuhlo_id, k.nimi AS vastuuhlo_nimi ' .
+        'FROM Elainkoelupa AS l ' .
+        'LEFT JOIN Kayttaja AS k ' .
+        'ON l.vastuuhlo_id = k.id ' .
+        'WHERE k.id = :kayttaja_id;');
+      $query->execute(array('kayttaja_id' => $kayttaja->id));
+
+      $rows = $query->fetchAll();
+      $luvat = array();
+
+      foreach($rows as $row) {
+        $luvat[] = new Elainkoelupa(array(
+          'id' => $row['id'],
+          'tunnus' => $row['tunnus'],
+          'nimi' => $row['nimi'],
+          'alkupvm' => $row['alkupvm'],
+          'loppupvm' => $row['loppupvm'],
+          'vastuuhlo_id' => $row['vastuuhlo_id'],
+          'vastuuhlo_nimi' => $row['vastuuhlo_nimi']
+        ));
+      }
+
+      return $luvat;
+    }
+
+    public function save() {
+      $query = DB::connection()->prepare(
+        'INSERT INTO Elainkoelupa (tunnus, nimi, alkupvm, loppupvm, vastuuhlo_id) VALUES ' .
+        '(:tunnus, :nimi, :alkupvm, :loppupvm, :vastuuhlo_id) RETURNING id;');
+      $query->execute(array('tunnus' => $this->tunnus,
+                            'nimi' => $this->nimi,
+                            'alkupvm' => $this->alkupvm,
+                            'loppupvm' => $this->loppupvm,
+                            'vastuuhlo_id' => $this->vastuuhlo_id));
+      $row = $query->fetch();
+      $this->id = $row['id'];
+    }
+
+    public function update() {
+      $query = DB::connection()->prepare(
+        'UPDATE Kayttaja SET ' .
+        '  tunnus = :tunnus, nimi = :nimi, alkupvm = :alkupvm ' .
+        '  loppupvm = :loppupvm, vastuuhlo_id = :vastuuhlo_id ' .
+        '  WHERE id = :id;');
+      $query->execute(array('tunnus' => $this->tunnus,
+                            'nimi' => $this->nimi,
+                            'alkupvm' => $this->alkupvm,
+                            'loppupvm' => $this->loppupvm,
+                            'vastuuhlo_id' => $this->vastuuhlo_id,
+                            'id' => $this->id));
+    }
+
+    public function destroy() {
+      // FIXME poista kokeet
+
+      $query = DB::connection()->prepare(
+        'DELETE FROM Elainkoelupa WHERE id = :id;');
+      $query->execute(array('id' => $this->id));
+    }
   }
