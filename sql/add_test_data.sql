a15f0de213c9244f0d3e04c3d28d69f71743295f
Aleksander Alafuzoff <aleksander.alafuzoff@gmail.com>
2017-10-10 13:14:06 +0300

    Lisätty uniikkius-rajoitteita ja foreign key poisto-rajoitteita, sekä nimetty Koeosallistuminen uudelleen

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,17 +1,16 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
-INSERT INTO Kayttaja (tunnus, salasana, nimi, email, oikeudet, status, lisayspvm) VALUES 
-  ('Yrmi', 'salainen', 'Yrmi Ylläpitäjä', 'yrmi.yllapitaja@yliopisto.fi', 'yllapitaja', 'true', '2017-09-01'), 
-  ('linda', 'salasana', 'Linda Lupavastaava', 'linda.lupavastaava@yliopisto.fi', 'lupavastaava', 'true', '2017-09-10'),
-  ('bella', '12345', 'Bella Biologi', 'bella.biologi@yliopisto.fi', 'tutkija', 'true', '2017-09-15');
-  
+INSERT INTO Kayttaja (tunnus, salasana, nimi, email, oikeudet, status, lisayspvm) VALUES
+  ('ylläpitaja', 'ylläpitaja', 'Ylläpitäjä', 'yllapitaja@yliopisto.fi', 'yllapitaja', 'true', '2017-09-01'),
+  ('lupavastaava', 'lupavastaava', 'Lupavastaava', 'lupavastaava@yliopisto.fi', 'lupavastaava', 'true', '2017-09-01'),
+  ('tutkija', 'tutkija', 'Tutkija', 'tutkija@yliopisto.fi', 'tutkija', 'true', '2017-09-01'),
+  ('test', 'test', 'Test', 'test@yliopisto.fi', 'yllapitaja', 'true', '2017-09-01');
+
 INSERT INTO Elainkoelupa (tunnus, nimi, alkupvm, loppupvm, vastuuhlo_id) VALUES
   ('ESAVI-12345', 'Rottien levitointitutkimus', '2017-06-01', '2019-06-01', (SELECT id FROM Kayttaja WHERE tunnus = 'linda'));
-  
+
 INSERT INTO Elainkoe (suorituspvm, laji, ika, lukumaara, lisatiedot, lupa_id) VALUES
   ('2017-09-15', 'rotta', 'Aikuinen', 3, 'Levitointi onnistunut!', (SELECT id FROM Elainkoelupa WHERE tunnus = 'ESAVI-12345'));
-  
-INSERT INTO Koeosallistuminen (koe_id, kayttaja_id) VALUES
+
+INSERT INTO Osallistuminen (koe_id, kayttaja_id) VALUES
   ((SELECT id FROM Elainkoe WHERE lisatiedot = 'Levitointi onnistunut!'), (SELECT id FROM Kayttaja WHERE tunnus = 'bella')),
   ((SELECT id FROM Elainkoe WHERE lisatiedot = 'Levitointi onnistunut!'), (SELECT id FROM Kayttaja WHERE tunnus = 'linda'));
-  
-  
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -4,7 +4,7 @@ CREATE TYPE elainlaji AS ENUM ('rotta', 'hiiri');
 
 CREATE TABLE Kayttaja (
   id        SERIAL PRIMARY KEY,
-  tunnus    VARCHAR(20) NOT NULL,
+  tunnus    VARCHAR(20) NOT NULL UNIQUE,
   salasana  VARCHAR(20) NOT NULL,
   nimi      VARCHAR(100) NOT NULL,
   email     VARCHAR(100) NOT NULL,
@@ -15,11 +15,11 @@ CREATE TABLE Kayttaja (
 
 CREATE TABLE Elainkoelupa (
   id           SERIAL PRIMARY KEY,
-  tunnus       VARCHAR(20) NOT NULL,
+  tunnus       VARCHAR(20) NOT NULL UNIQUE,
   nimi         VARCHAR(100) NOT NULL,
   alkupvm      DATE,
   loppupvm     DATE,
-  vastuuhlo_id INTEGER REFERENCES Kayttaja(id)
+  vastuuhlo_id INTEGER REFERENCES Kayttaja(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Elainkoe (
@@ -29,11 +29,11 @@ CREATE TABLE Elainkoe (
   ika         VARCHAR(20),
   lukumaara   INTEGER,
   lisatiedot  VARCHAR(500),
-  lupa_id     INTEGER REFERENCES Elainkoelupa(id)
+  lupa_id     INTEGER REFERENCES Elainkoelupa(id) ON DELETE CASCADE
 );
 
-CREATE TABLE Koeosallistuminen (
-  koe_id      INTEGER REFERENCES Elainkoe(id),
-  kayttaja_id INTEGER REFERENCES Kayttaja(id),
+CREATE TABLE Osallistuminen (
+  koe_id      INTEGER REFERENCES Elainkoe(id) ON DELETE CASCADE,
+  kayttaja_id INTEGER REFERENCES Kayttaja(id) ON DELETE CASCADE,
   PRIMARY KEY(koe_id, kayttaja_id)
 );
